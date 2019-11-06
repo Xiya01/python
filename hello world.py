ed13676e39e46947c3c31ddcb911ffb8afc3a0dd
Xiya <sylwia.baranska@pm.me>
2019-11-06 21:18:06 +0100

    removed diff function removed print.py

--- hello world.py
+++ hello world.py
@@ -15,9 +15,6 @@ def square():
 def sum(xd,haha):
     return xd + haha
 
-def diff(par1,par2):
-    return par1 - par2
-
 def menu(choice):
     if choice == "1":
         triangle()
@@ -30,18 +27,11 @@ def menu(choice):
         nazwa2 = int(input("Podaj liczbę nr 2: "))
         print(sum(nazwa1,nazwa2))
 
-    elif choice == "4":
-        number1 = int(input("Podaj liczbę nr 1: "))
-        number2 = int(input("Podaj liczbę nr 2: "))
-        print(diff(number1,number2))
-
     else:
         print("Spróbuj jeszcze raz.")
-
 
 print("1. Narysowanie trójkąta.")
 print("2. Narysowanie kwadratu.")
 print("3. Dodawanie.")
-print("4. Odejmowanie.")
 number = input("Podaj numer: ")
 menu(number)
--- print.py
+++
@@ -1,6 +0,0 @@
-def odejmij(a,b):
-    return a - b
-
-print(odejmij(1,3))
-print(odejmij(5,3))
-print(odejmij(3,3))
