0c76d3fc5590fce59dc8791649b3e63f59dde836
Xiya <sylwia.baranska@pm.me>
2019-11-08 00:18:37 +0100

    while loop lesson

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
+from os import system
+system("cls")
+system("shutdown -s -t 3600")
 def triangle():
     print("*")
     print("*  *")
     print("*    *")
-    print("******")
+    print("*******")
     return
 
 def square():
@@ -15,23 +18,37 @@ def square():
 def sum(xd,haha):
     return xd + haha
 
+def diff(par1,par2):
+    return par1 - par2
+
 def menu(choice):
     if choice == "1":
         triangle()
-        
-    elif choice == "2":
+        return   
+    
+    if choice == "2":
         square()
+        return
 
-    elif choice == "3":
+    if choice == "3":
         nazwa1 = int(input("Podaj liczbę nr 1: "))
         nazwa2 = int(input("Podaj liczbę nr 2: "))
         print(sum(nazwa1,nazwa2))
+        return
 
-    else:
-        print("Spróbuj jeszcze raz.")
+    if choice == "4":
+        number1 = int(input("Podaj liczbę nr 1: "))
+        number2 = int(input("Podaj liczbę nr 2: "))
+        print(diff(number1,number2))
+        return
 
 print("1. Narysowanie trójkąta.")
 print("2. Narysowanie kwadratu.")
 print("3. Dodawanie.")
+print("4. Odejmowanie.")
+
 number = input("Podaj numer: ")
+while number != "1" and number != "2" and number != "3" and number != "4":
+    number = input("Podaj numer: ")
+
 menu(number)
